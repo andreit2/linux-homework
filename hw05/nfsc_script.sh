e0ebd007a8338b8f9827e304c091a307f77975fa
Andrii Trofymchuk <50780083+andreit2@users.noreply.github.com>
2021-06-16 15:11:43 +0300

    Update nfsc_script.sh

--- hw05/nfsc_script.sh
+++ hw05/nfsc_script.sh
@@ -4,4 +4,4 @@ sudo systemctl enable firewalld
 sudo mkdir /mnt/share_storage
 sudo mount -t nfs -o rw,nosuid,noexec,soft,intr,proto=udp,vers=3 192.168.50.10:/mnt/share_storage /mnt/share_storage
 echo "192.168.50.10:/mnt/share_storage /mnt/share_storage nfs noauto,x-systemd.automount,proto=udp,vers=3 0 0" >> /etc/fstab
-#reboot
+
