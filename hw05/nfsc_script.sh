38842fe1975316e33df9f49c7c25315fa934e183
Andrii Trofymchuk <50780083+andreit2@users.noreply.github.com>
2021-06-16 11:02:12 +0300

    Update nfsc_script.sh

--- hw05/nfsc_script.sh
+++ hw05/nfsc_script.sh
@@ -5,5 +5,5 @@ sudo systemctl enable rpcbind
 sudo systemctl start firewalld
 sudo systemctl enable firewalld
 sudo mkdir /mnt/share_storage
-sudo mount -o nfsvers=3  192.168.50.10:/mnt/share_storage /mnt/share_storage
-echo "192.168.50.10:/mnt/share_storage/ /mnt/share_storage/ nfs nfsvers=3 0 0" >> /etc/fstab
+sudo mount -o nfsvers=3,udp 192.168.50.10:/mnt/share_storage /mnt/share_storage
+echo "192.168.50.10:/mnt/share_storage/ /mnt/share_storage/ nfs nfsvers=3,udp 0 0" >> /etc/fstab
