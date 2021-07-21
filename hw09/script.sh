9ec23cd4127f5803676cc5fcdba6cdb1fdf58b8c
Andrii Trofymchuk <50780083+andreit2@users.noreply.github.com>
2021-07-21 22:31:34 +0300

    Update script.sh

--- hw09/script.sh
+++ hw09/script.sh
@@ -7,19 +7,19 @@ Y=10
 # Указываем имя файла
 F=access-4560-644067.log
 parser() {
-echo "Request generated:" && date && echo "Log started:" && cat $F | awk '{print $4$5}' | head -n 1
-echo "-----------------------------------------------------"
+echo "Request generated:" && date && echo "Log started:" && cat $F | awk '{print $4$5}' | head -n 1 >> mail.txt
+echo "-----------------------------------------------------"  >> mail.txt
 #X IP адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта
-echo $X "IP adrreses with the most requestes :" && cat $F | awk -F" " '{print $1}' | sort | uniq -c | sort -rn | head -n $X | awk '{print $2 " had " $1 " requests"}' | column -t 
-echo "-----------------------------------------------------"
+echo $X "IP adrreses with the most requestes :" && cat $F | awk -F" " '{print $1}' | sort | uniq -c | sort -rn | head -n $X | awk '{print $2 " had " $1 " requests"}' | column -t >> mail.txt
+echo "-----------------------------------------------------" >> mail.txt
 #Y запрашиваемых адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта
-echo $Y "most requested URLs:" && cat $F | awk '{print $7}' | sort | uniq -c | sort -rn | head -n $Y | awk '{print $2 " requested " $1 " times. "}' | column -t
-echo "-----------------------------------------------------"
+echo $Y "most requested URLs:" && cat $F | awk '{print $7}' | sort | uniq -c | sort -rn | head -n $Y | awk '{print $2 " requested " $1 " times. "}' | column -t >> mail.txt
+echo "-----------------------------------------------------" >> mail.txt
 #все ошибки c момента последнего запуска
-echo "All errors:" && cat $F | awk '{print $9}' | grep ^4 | sort | uniq -c | sort -rn | awk '{print $2 " was " $1 " times."}' | column -t && cat $F | awk '{print $9}' | grep ^5 | sort | uniq -c | sort -rn | awk '{print $2 " was " $1 " times."}' | column -t
-echo "-----------------------------------------------------"
+echo "All errors:" && cat $F | awk '{print $9}' | grep ^4 | sort | uniq -c | sort -rn | awk '{print $2 " was " $1 " times."}' | column -t && cat $F | awk '{print $9}' | grep ^5 | sort | uniq -c | sort -rn | awk '{print $2 " was " $1 " times."}' | column -t >> mail.txt
+echo "-----------------------------------------------------" >> mail.txt
 #список всех кодов возврата с указанием их кол-ва с момента последнего запуска
-echo "HTTP statuses:" && cat $F | awk '{print $9}'| grep -v "-" | sort | uniq -c | sort -rn | awk '{print $2 " was " $1 " times."}' | column -t
+echo "HTTP statuses:" && cat $F | awk '{print $9}'| grep -v "-" | sort | uniq -c | sort -rn | awk '{print $2 " was " $1 " times."}' | column -t >> mail.txt
 }
 start() {
     if ( set -o noclobber; echo `ps -a | grep script.sh`  > "$lockfile") 2> /dev/null;
