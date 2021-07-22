964768527709a2af46d4b67e2221722562d52404
Andrii Trofymchuk <50780083+andreit2@users.noreply.github.com>
2021-07-22 16:36:35 +0300

    Update vagrant.sh

--- hw09/vagrant.sh
+++ hw09/vagrant.sh
@@ -3,4 +3,4 @@ cd /opt
 git clone https://github.com/andreit2/linux-homework.git
 cd linux-homework/hw09/
 sudo chmod a+x script.sh
-sudo echo "* 0-23\1 * * * root /opt/linux-homework/hw09/script.sh" > /var/spool/cron/root
+sudo echo "0 */1 * * * /opt/linux-homework/hw09/script.sh" > /var/spool/cron/root
