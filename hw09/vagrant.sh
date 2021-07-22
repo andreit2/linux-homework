67e546de5f52be899ab455a118bc648376ebade4
Andrii Trofymchuk <50780083+andreit2@users.noreply.github.com>
2021-07-22 10:50:37 +0300

    Update vagrant.sh

--- hw09/vagrant.sh
+++ hw09/vagrant.sh
@@ -1,5 +1,5 @@
 sudo yum install -y git
-git clone git@github.com:andreit2/linux-homework.git
+git clone https://github.com/andreit2/linux-homework.git
 cd linux-homework/hw09/
 sudo chmod a+x script.sh
 sudo echo "00 * * * * root /home/vagrant/linux-homework/hw09/script.sh" > /etc/cron.d/0hourly
