fe4da21283efdee756cce1fb35f57966a7e9f00b
Andrii Trofymchuk <50780083+andreit2@users.noreply.github.com>
2021-07-22 10:15:06 +0300

    Update script.sh

--- hw09/script.sh
+++ hw09/script.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-lockfile=/tmp/lockfile
 
-#Объявляем значение X и Y
+
+#Переменные
+lockfile=./lockfile
 X=10
 Y=10
-# Указываем имя файла
 F=access-4560-644067.log
+env=.env
+timestamp=`cat $env | grep timestamp | awk -F"=" '{ print $2}'`
+number_previous=`cat $env | grep number | awk -F"=" '{ print $2}'`
+number_actual=`cat $F | wc -l`
 parser() {
-echo "Request generated:" && date && echo "Log started:" && cat $F | awk '{print $4$5}' | head -n 1
-echo "-----------------------------------------------------"
+#echo "Request generated:" && date && echo "Log started:" && cat $F | awk '{print $4$5}' | head -n 1
+#echo "-----------------------------------------------------"
 #X IP адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта
 echo $X "IP adrreses with the most requestes :" && cat $F | awk -F" " '{print $1}' | sort | uniq -c | sort -rn | head -n $X | awk '{print $2 " had " $1 " requests"}' | column -t
 echo "-----------------------------------------------------"
@@ -21,13 +25,32 @@ echo "-----------------------------------------------------"
 #список всех кодов возврата с указанием их кол-ва с момента последнего запуска
 echo "HTTP statuses:" && cat $F | awk '{print $9}'| grep -v "-" | sort | uniq -c | sort -rn | awk '{print $2 " was " $1 " times."}' | column -t
 }
+time() {
+    date_actual=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "Request generated: $timestamp - $date_actual" >> mail.txt
+    sed -i "s/timestamp=.*/timestamp=$date_actual/g" $env
+}
+log() {
+    if (($number_previous > $number_actual));
+	then
+	    number_previous=0
+	    let n=$number_actual
+	else
+	    let n=$number_actual-$number_previous
+	    let number_previous=$number_actual-$n+1
+    fi
+    sed -i "s/num=.*/num=$num_curr/g" $envfile
+}
+
 start() {
     if ( set -o noclobber; echo `ps -a | grep script.sh`  > "$lockfile") 2> /dev/null;
         then
         trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT
         while true
             do
-            parser >> mail2.txt
+            log
+            parser >> mail.txt
+            mail -s "Statistic" root@localhost < mail.txt
             exit
          done
          rm -f "$lockfile"
